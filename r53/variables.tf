380a41baac3c5b5260ae3dfd640236747344a356
Darren Pham <phamdarren1@gmail.com>
2020-12-08 20:17:22 -0800

    foreach function r53 hostnames to same alb variable fix

--- r53/variables.tf
+++ r53/variables.tf
@@ -2,7 +2,7 @@ variable domain_name {
   description = "The domain name where the application will be deployed, must already live in AWS"
 }
 
-variable host_name {
+variable host_names {
   description = "The URL where the application will be hosted, must be a subdomain of the domain_name"
   type = list(string)
 }
